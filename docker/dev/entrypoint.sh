8c5f045af4963d8d290b87f59a4eea947edc61e6
KZ08EY <kz08ey@student.aau.dk@es-dpu-01.srv.aau.dk>
2026-03-29 18:18:35 +0200

    include ucx libs in dockerfiles

--- docker/dev/entrypoint.sh
+++ docker/dev/entrypoint.sh
@@ -2,5 +2,5 @@
 export GVIRTUS_LOGLEVEL=${GVIRTUS_LOGLEVEL:-20000}  # default to INFO if not set
 
 mkdir gvirtus/build && cd gvirtus/build && cmake .. && make -j$(nproc) && make install
- ${GVIRTUS_HOME}/bin/gvirtus-backend ${GVIRTUS_HOME}/etc/properties.json
+ ${GVIRTUS_HOME}/bin/gvirtus-backend ${GVIRTUS_HOME}/etc/properties_ucx.json
 #tail -f /dev/null # for debugging
