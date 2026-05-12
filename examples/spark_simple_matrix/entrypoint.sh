b94a5397c54ac5124be930c1c0481a2418f7ab58
WG38UP <wg38up@student.aau.dk@es-dpu-02.srv.aau.dk>
2026-05-12 13:18:09 +0200

    changes

--- examples/spark_simple_matrix/entrypoint.sh
+++ examples/spark_simple_matrix/entrypoint.sh
@@ -2,7 +2,7 @@
 set -e
 
 GVIRTUS_HOME="${GVIRTUS_HOME:-/opt/GVirtuS}"
-RAPIDS_JAR="/app/jars/rapids-4-spark_2.12-26.02.1.jar"
+RAPIDS_JAR="/app/jars/rapids-4-spark_2.13-26.04.0.jar"
 
 cd /app/src
 exec python3 simple_matrix.py "$@"
